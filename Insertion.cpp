fbabfe358ef021d961d9c41e094c08d8646de1c1
최가온 <choigaon1028@hanyang.ac.kr>
2020-10-05 01:06:47 +0900

    Insertion sort

--- Insertion.cpp
+++ Insertion.cpp
@@ -1,28 +1,31 @@
-#include <stdio.h>
+// 2019009261_ÃÖ°¡¿Â_12838
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int arr[30001];
-	int n, key;
-	scanf("%d", &n);
+int n, k, cnt = 0;
+int v[100001];
 
-	int i = 0, j;
-	
-	while (i < n) {
-		scanf("%d", &arr[i]);
-		key = arr[i];
-		j = i - 1;
-
-		while (arr[j] < key && j >= 0) {
-			arr[j + 1] = arr[j];
-			j = j - 1;
+void insert(int data) {
+	v[cnt++] = data;
+	int t = cnt;
+	while (t != 1) {
+		if (v[t] > v[t / 2]) {
+			swap(&v[t], &v[t / 2]);
+			t /= 2;
 		}
-		arr[j + 1] = key;
-		i++;
+		else break;
 	}
+}
 
-	i = 0;
-	while (i < n)
-		printf("%d\n", arr[i++]);
+int main() {
+	scanf("%d %d", &n, &k);
+	int data_;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &data_);
+		insert(data_);
+	}
+	for (int i = 0; i < n; i++)
+		printf("%d ", v[i]);
 	return 0;
 }
